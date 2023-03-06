e07719f8a0afa220108ee6f2653d9c551e51b1cc
davidson <davidson031@hotmail.com>
2023-03-06 10:49:55 -0300

    html e css do componente de game

--- src/App.js
+++ src/App.js
@@ -18,11 +18,39 @@ const stages = [
 
 function App() {
 
+  
+
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const  pickWordAndCategory = () => {
+
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+  
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+  
+    return {word, category};
+  };
+
   //iniciar game
   const startGame = () => {
+    
+    //pick word and pick category
+    const { word, category } = pickWordAndCategory();
+
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((e) => e.toLowerCase());
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
     setGameStage(stages[1].name);
   };
 
@@ -39,7 +67,7 @@ function App() {
   return (
     <div className="App">
       {gameStage === "start" && <StartScreen startGame={ startGame } />}
-      {gameStage === "game" && <Game verifyLetter = { verifyLetter } /> }
+      {gameStage === "game" && <Game verifyLetter = { verifyLetter }/> }
       {gameStage === "end" &&  <GameOver retry = { retry } />}
     </div>
   );
